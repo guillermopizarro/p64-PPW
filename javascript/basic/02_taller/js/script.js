d39c8c4d04772f83cab5baaa1bb83d4294c2c993
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2024-06-22 12:45:20 -0500

    Creacion y eliminacion de elementos HTML mediante Javascript DOM

---
+++ javascript/basic/02_taller/js/script.js
@@ -0,0 +1,19 @@
+var contador = 1
+
+function cargar_elemento() {
+    var content = document.getElementById("content")
+    
+    var div = document.createElement("div")
+    div.className = "item-box"
+    var texto = document.createTextNode("Item " + contador)
+    contador++
+    div.appendChild(texto)
+
+    content.appendChild(div)
+}
+
+function eliminar_elemento() {
+    var content = document.getElementById("content")
+    content.removeChild(content.lastElementChild)
+    contador--
+}
