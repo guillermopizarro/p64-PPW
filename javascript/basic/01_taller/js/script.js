2c87a15b54cab2a60b211011477d4330d5750fb6
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2024-06-22 12:33:51 -0500

    Creacion de elementos HTML mediante Javascript

--- javascript/basic/01_taller/js/script.js
+++ javascript/basic/01_taller/js/script.js
@@ -3,5 +3,11 @@ var contador = 1
 function cargar_elemento() {
     var content = document.getElementById("content")
     
+    var div = document.createElement("div")
+    div.className = "item-box"
+    var texto = document.createTextNode("Item " + contador)
+    contador++
+    div.appendChild(texto)
 
+    content.appendChild(div)
 }
