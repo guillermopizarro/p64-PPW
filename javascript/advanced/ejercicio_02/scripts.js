e6524d573df2661a07b6345225ec0ef09692fb0b
guillermopizarro <gpizarro@espol.edu.ec>
2024-07-20 12:59:04 -0500

    API de Marvel

--- javascript/advanced/ejercicio_02/scripts.js
+++ javascript/advanced/ejercicio_02/scripts.js
@@ -1,10 +1,26 @@
-const URL = 'https://rickandmortyapi.com/api/character/'
+const URL = "https://gateway.marvel.com:443/v1/public/characters";
 var xhttp = null
+
+const publicKey = "ea1bdd33bc6e4f522865590d3437966a";
+const privateKey = "d64650e4aae877bc3c07b56d1636664a910bd2e7";
+
+// Generar ts
+var ts = new Date().getTime().toString();
+// Generar hash md5
+var hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+
+var params = {
+    ts: ts,
+    apikey: publicKey,
+    hash: hash,
+    limit: 10  // Por ejemplo, obtener los primeros 10 personajes
+};
+
 
 function conectar(personaje) {
     xhttp = new XMLHttpRequest()
+    xhttp.open('GET', URL + '?' + params.toString)
     xhttp.onreadystatechange = obtener_respuesta
-    xhttp.open('GET', URL + personaje, true)
     xhttp.send()
 }
 
@@ -14,10 +30,12 @@ function obtener_respuesta() {
             let dato =  JSON.parse(xhttp.responseText)
             console.log(dato)
 
-            let container = document.getElementById('container')
+            /*let container = document.getElementById('container')
             let personaje = `<div class="container-item"><h1>${dato.name}</h1>
             <img src="${dato.image}" /><div class="container-item">`
-            container.innerHTML = personaje
+            container.innerHTML = personaje*/
+        } else {
+            console.log(xhttp)
         }
     }
 }
