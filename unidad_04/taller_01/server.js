d42583c6c51d17dfa7f64938a89525e258b732d3
guillermopizarro <gpizarro@espol.edu.ec>
2024-08-03 11:43:46 -0500

    Server basico

---
+++ unidad_04/taller_01/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+var app = express()
+
+app.use('/', function(req, res){
+    res.send('HOLA MUNDO!!!')
+})
+
+let puerto = 3000
+app.listen(puerto)
+console.log(`La aplicación se encuentra funcionando en http://localhost:${puerto}/`)
